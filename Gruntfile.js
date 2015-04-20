105f6c5b18f0994f944fd056f13d1b532049f2fa
rmallols <ricardo.mallols@gmail.com>
2015-04-20 19:52:59 +0100

    Pushing local resources after deploying to test

--- Gruntfile.js
+++ Gruntfile.js
@@ -269,5 +269,5 @@ module.exports = function (grunt) {
 
     grunt.registerTask('dev', ['setDevDb', 'cleanDist', 'setDevLoader']);
     grunt.registerTask('test', ['setTestDb', 'cleanDist', 'setProdLoader', 'optimizeJs',
-        'optimizeSvg', 'compileLess', 'githubPush', 'herokuPush', 'dev']);
+        'optimizeSvg', 'compileLess', 'githubPush', 'herokuPush', 'dev', 'githubPush']);
 };
